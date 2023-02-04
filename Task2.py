b9e419e2229f662dc87b2908cfd686588c97142d
Igor <you@example.com>
2023-02-04 11:57:51 +0300

    Add corrections Task2

--- Task2.py
+++ Task2.py
@@ -7,12 +7,10 @@
 Реализуйте с list comprehension
 """
 
-list_in = [12, 44, 4, 10, 78, 123]
-list_out = []
+list_in = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
 list_out = [
-    list_in[i] for i in range(len(list_in))
-    if list_in[i] > list_in[i - 1] or i == 0
+    list_in[i] for i in range(1, len(list_in)) if list_in[i] > list_in[i - 1]
 ]
 
 print(f'Исходный список\n{list_in}')
